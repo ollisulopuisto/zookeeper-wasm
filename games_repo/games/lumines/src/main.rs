f39958e78eaa2b33f135839ee9136e2f6acde57e
Olli Sulopuisto <dst@iki.fi>
2026-04-05 12:52:38 +0300

    chore: bump version after rebase

--- games_repo/games/lumines/src/main.rs
+++ games_repo/games/lumines/src/main.rs
@@ -9,7 +9,7 @@ use audio::AudioManager;
 
 const COLS: usize = 16;
 const ROWS: usize = 10;
-const VERSION: &str = "26.04.05.218";
+const VERSION: &str = "26.04.05.219";
 
 const BEATS_PER_SWEEP: f32 = 8.0;
 const FREEZE_DURATION: f32 = 4.0;
